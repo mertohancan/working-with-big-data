002360124829c4fdcc1debeac5ccb7e9b47a0b3a
Mert Ohancan <mert.ohancan@setur.com.tr>
2024-02-16 17:18:56 +0300

    Convert fetch api to the hooks

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,29 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import DataTable from '../../components/data-table';
 import { SelectedBet, useBet } from '../../contexts/bet';
-import { Bet } from '../../types/bet';
 import BasketComponent from '../../components/basket';
+import { useGetBets } from '../../hooks/useGetBets';
 
 const Home = () =>{
-  const [bets, setBets] = useState<Bet[]>([]);
+  const { bets } = useGetBets();
   const { state, dispatch } = useBet();
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch("https://nesine-case-study.onrender.com/bets");
-        if (!response.ok) {
-          throw new Error('Veri alınamadı.');
-        }
-        const data = await response.json();
-        setBets(data);
-      } catch (error) {
-        console.error('Hata:', error);
-      }
-    };
-
-    fetchData();
-  }, []); 
 
   const handleSelectBet = (bet: SelectedBet) => {
     dispatch({ type: 'ADD_BET', bet: bet });
